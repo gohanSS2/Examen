d4ca6c8225f15b5154a13db34d8908105e5c04f9
itoro <itoro@106-09.info2>
2012-05-21 13:32:11 +0200

    Commit rama clase 2

--- itoro/src/clase2.java
+++ itoro/src/clase2.java
@@ -6,4 +6,7 @@ public class clase2 {
 	public void metodo2_clase2(){
 		System.out.println("Primer método 2.0");
 	}
+	public void metodo_rama_master_clase2(){
+		System.out.println("Primer método rama 2.0");
+	}
 }
